ce4a78446e8a14cc6a6c95bf7646dcb29e105cee
Suzan van Borkum <suzan.vanborkum@gmail.com>
2020-08-25 21:39:50 -0500

    some changes in models to mak the creation of categories work

--- app/models/protest.rb
+++ app/models/protest.rb
@@ -1,10 +1,11 @@
 class Protest < ApplicationRecord
   belongs_to :station
-  has_many :protest_assaults
+  has_many :protest_assaults, dependent: :destroy
   has_many :assault_categories, through: :protest_assaults
 
   validates :experienced, inclusion: { in: [true, false] }
   validates :date, presence: true
   validates :description, presence: true
   validates :station, presence: true
+
 end
--- app/models/protest_assault.rb
+++ app/models/protest_assault.rb
@@ -1,4 +1,6 @@
 class ProtestAssault < ApplicationRecord
   belongs_to :protest
   belongs_to :assault_category
+
+  validates_uniqueness_of :protest, :scope => [:assault_category]
 end
