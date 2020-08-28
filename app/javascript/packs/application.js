2938a605a63c5710ce24339ed9f72d6a05510271
Nina Herrera <ninaeherrera@gmail.com>
2020-08-28 15:41:50 -0500

    work on new form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,8 @@ require("@rails/activestorage").start()
 require("channels")
 
 
-import "animate.css"
+import "animate.css";
+import '@ttskch/select2-bootstrap4-theme/dist/select2-bootstrap4.css'
 import "bootstrap";
 import { initMapbox } from '../plugins/init_mapbox';
 import { initSelect2 } from '../plugins/init_select2';
--- app/javascript/plugins/init_select2.js
+++ app/javascript/plugins/init_select2.js
@@ -3,7 +3,9 @@ import 'select2';
 
 const initSelect2 = () => {
   console.log()
-  $('#protest_station_id').select2();
+  $('#protest_station_id').select2({
+    theme: 'bootstrap4', placeholder: "Select station", width: "100%",
+    });
 };
 
 export { initSelect2 };
