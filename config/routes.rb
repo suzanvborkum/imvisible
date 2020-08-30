675aa781e21d8463a06fa3cff24633d432836c83
Suzan van Borkum <suzan.vanborkum@gmail.com>
2020-08-30 12:19:52 -0500

    setup home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :protests, only: %i[index show new create] do
     resources :protest_assaults, only: :create
+    collection do
+      get :today
+    end
   end
 end
