b4bc47d914de2b13579affaa57f1781d0228168b
Eduardo Betancourt <ebhdz@hotmail.com>
2020-09-22 16:36:58 -0500

    Whole page is completely navigatable in spanish

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ ProtestAssault.destroy_all
 AssaultCategory.destroy_all
 Station.destroy_all
 
-assault_categories = %w(verbal touching following photos catcalling rape)
+assault_categories = ["verbal", "me siguieron", "toqueteo", "fotos", "piropos", "violación"]
 assault_categories.each do |category|
   AssaultCategory.create!(name: category)
 end
