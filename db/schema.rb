859bc8cb93bd3b91859d0e12bc0807a8c26846e6
Eduardo Betancourt <ebhdz@hotmail.com>
2020-09-25 13:41:54 -0500

    changed a typo

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_09_24_165744) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,8 +38,8 @@ ActiveRecord::Schema.define(version: 2020_09_24_165744) do
     t.bigint "station_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "age"
     t.string "other_category"
+    t.integer "age"
     t.index ["station_id"], name: "index_protests_on_station_id"
   end
 
