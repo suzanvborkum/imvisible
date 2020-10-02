b5a4af8611322b7b3c321b7d9a6ad90a2bfbcd9e
Eduardo Betancourt <ebhdz@hotmail.com>
2020-10-02 18:45:00 -0500

    created footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   def home
     @protests = Protest.all
-    @count_protests = Protest.where(date: Date.today).count
-    @count_locations = Protest.group(:station).where(date: Date.today).count.count
+    @count_protests = Protest.where(created_at: Date.today).count
+    @count_locations = Protest.group(:station).where(created_at: Date.today).count.count
     @count_shares = rand(6..14)
   end
 
