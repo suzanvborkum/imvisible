38b8b51d9862731d509d1fe22005fa30020cf272
Suzan van Borkum <suzan.vanborkum@gmail.com>
2020-10-03 13:17:55 -0500

    changed the protest today counter and location counter to protests that were created today

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   def home
     @protests = Protest.all
-    @count_protests = Protest.where(date: Date.today).count
-    @count_locations = Protest.group(:station).where(date: Date.today).count.count
+    @count_protests = Protest.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).count
+    @count_locations = Protest.group(:station).where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).count.count
     @count_shares = rand(6..14)
   end
 
