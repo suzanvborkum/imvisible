d9df1e25e831acc226189ceb65b520d878955198
Eduardo Betancourt <ebhdz@hotmail.com>
2020-10-03 13:54:49 -0500

    added route to support page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     root to: 'pages#home'
     get '/about', to: 'pages#about'
+    get '/support', to: 'pages#support'
     resources :protests, only: %i[index show new create] do
       resources :protest_assaults, only: :create
     end
