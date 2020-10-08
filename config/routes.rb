b8072324d40647be4710004205c3125b67d0f81e
Suzan van Borkum <suzan.vanborkum@gmail.com>
2020-10-08 11:30:58 -0500

    installed rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     root to: 'pages#home'
